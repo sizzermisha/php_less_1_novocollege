59c432c9b457fde548c51c0f0da00d2625fdc690
Misha Vayman <misha.cerez@gmail.com>
2023-12-15 10:21:28 +0300

    add php files, recorection

---
+++ elements/header.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>News HTML-5 Template </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="manifest" href="site.webmanifest">
+		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+		<!-- CSS here -->
+            <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+            <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+            <link rel="stylesheet" href="assets/css/ticker-style.css">
+            <link rel="stylesheet" href="assets/css/flaticon.css">
+            <link rel="stylesheet" href="assets/css/slicknav.css">
+            <link rel="stylesheet" href="assets/css/animate.min.css">
+            <link rel="stylesheet" href="assets/css/magnific-popup.css">
+            <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+            <link rel="stylesheet" href="assets/css/themify-icons.css">
+            <link rel="stylesheet" href="assets/css/slick.css">
+            <link rel="stylesheet" href="assets/css/nice-select.css">
+            <link rel="stylesheet" href="assets/css/style.css">
+   </head>
+   <body>
+		<header>
+			<!-- Header Start -->
+		<div class="header-area">
+				<div class="main-header ">
+					<div class="header-top black-bg d-none d-md-block">
+					<div class="container">
+						<div class="col-xl-12">
+								<div class="row d-flex justify-content-between align-items-center">
+									<div class="header-info-left">
+										<ul>     
+											<li><img src="assets/img/icon/header_icon1.png" alt="">34ºc, Sunny </li>
+											<li><img src="assets/img/icon/header_icon1.png" alt="">Tuesday, 18th June, 2019</li>
+										</ul>
+									</div>
+									<div class="header-info-right">
+										<ul class="header-social">    
+											<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+											<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+										<li> <a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+										</ul>
+									</div>
+								</div>
+						</div>
+					</div>
+					</div>
+					<div class="header-mid d-none d-md-block">
+					<div class="container">
+							<div class="row d-flex align-items-center">
+								<!-- Logo -->
+								<div class="col-xl-3 col-lg-3 col-md-3">
+									<div class="logo">
+										<a href="index.php"><img src="assets/img/logo/logo.png" alt=""></a>
+									</div>
+								</div>
+								<div class="col-xl-9 col-lg-9 col-md-9">
+									<div class="header-banner f-right ">
+										<img src="assets/img/hero/header_card.jpg" alt="">
+									</div>
+								</div>
+							</div>
+					</div>
+					</div>
+				<div class="header-bottom header-sticky">
+						<div class="container">
+							<div class="row align-items-center">
+								<div class="col-xl-10 col-lg-10 col-md-12 header-flex">
+									<!-- sticky -->
+										<div class="sticky-logo">
+											<a href="index.php"><img src="assets/img/logo/logo.png" alt=""></a>
+										</div>
+									<!-- Main-menu -->
+									<div class="main-menu d-none d-md-block">
+										<nav>                  
+											<ul id="navigation">    
+												<li><a href="index.php">Home</a></li>
+												<li><a href="categori.php">Category</a></li>
+												<li><a href="about.php">About</a></li>
+												<li><a href="latest_news.php">Latest News</a></li>
+												<li><a href="contact.php">Contact</a></li>
+												<li><a href="#">Pages</a>
+													<ul class="submenu">
+														<li><a href="elements.php">Element</a></li>
+														<li><a href="blog.php">Blog</a></li>
+														<li><a href="single-blog.php">Blog Details</a></li>
+														<li><a href="details.php">Categori Details</a></li>
+													</ul>
+												</li>
+											</ul>
+										</nav>
+									</div>
+								</div>             
+								<div class="col-xl-2 col-lg-2 col-md-4">
+									<div class="header-right-btn f-right d-none d-lg-block">
+										<i class="fas fa-search special-tag"></i>
+										<div class="search-box">
+											<form action="#">
+												<input type="text" placeholder="Search">
+												
+											</form>
+										</div>
+									</div>
+								</div>
+								<!-- Mobile Menu -->
+								<div class="col-12">
+									<div class="mobile_menu d-block d-md-none"></div>
+								</div>
+							</div>
+						</div>
+				</div>
+				</div>
+		</div>
+			<!-- Header End -->
+   		</header>
+			<!-- JS here -->
+	
+		<!-- All JS Custom Plugins Link Here here -->
+        <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+		<!-- Jquery, Popper, Bootstrap -->
+		<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="./assets/js/popper.min.js"></script>
+        <script src="./assets/js/bootstrap.min.js"></script>
+	    <!-- Jquery Mobile Menu -->
+        <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+		<!-- Jquery Slick , Owl-Carousel Plugins -->
+        <script src="./assets/js/owl.carousel.min.js"></script>
+        <script src="./assets/js/slick.min.js"></script>
+        <!-- Date Picker -->
+        <script src="./assets/js/gijgo.min.js"></script>
+		<!-- One Page, Animated-HeadLin -->
+        <script src="./assets/js/wow.min.js"></script>
+		<script src="./assets/js/animated.headline.js"></script>
+        <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+        <!-- Breaking New Pluging -->
+        <script src="./assets/js/jquery.ticker.js"></script>
+        <script src="./assets/js/site.js"></script>
+
+		<!-- Scrollup, nice-select, sticky -->
+        <script src="./assets/js/jquery.scrollUp.min.js"></script>
+        <script src="./assets/js/jquery.nice-select.min.js"></script>
+		<script src="./assets/js/jquery.sticky.js"></script>
+        
+        <!-- contact js -->
+        <script src="./assets/js/contact.js"></script>
+        <script src="./assets/js/jquery.form.js"></script>
+        <script src="./assets/js/jquery.validate.min.js"></script>
+        <script src="./assets/js/mail-script.js"></script>
+        <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+        
+		<!-- Jquery Plugins, main Jquery -->	
+        <script src="./assets/js/plugins.js"></script>
+        <script src="./assets/js/main.js"></script>
+	</body>
+</html>
